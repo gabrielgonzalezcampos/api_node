d9e1789054aa1d1f2990514a12f5753247cabc31
GABRI <gabrigoca@gmail.com>
2019-03-04 12:08:51 +0100

    Fist commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express=require('express')
+const productCtrl =require('../controllers/product')
+const api = express.Router()
+const auth = require('../middlewares/auth')
+
+api.get('/product', productCtrl.getProducts)
+api.get('/product/:productId', productCtrl.getProduct)
+api.post('/product', productCtrl.saveProduct)
+api.put('/product/:productId', productCtrl.updateProduct)
+api.delete('/product/:productId', productCtrl.deleteProduct)
+
+api.get('/private', auth.isAuth, (req, res) => {
+    res.status(200).send({ message: 'Tienes acceso' })
+})
+
+module.exports = api
